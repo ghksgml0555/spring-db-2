0cc3d8922e47f85de13319a7d9e35e55037f06b3
dsad2221s <ghksgml0555@naver.com>
2023-03-11 18:06:55 +0900

    Item 객체를 jpa가 인식하도록 애노테이션 추가
    Item 객체를 jpa가 인식하도록 애노테이션 추가

--- itemservice-db/src/main/java/hello/itemservice/domain/Item.java
+++ itemservice-db/src/main/java/hello/itemservice/domain/Item.java
@@ -2,12 +2,20 @@ package hello.itemservice.domain;
 
 import lombok.Data;
 
+import javax.persistence.*;
+
 @Data
+@Entity //JPA에서 관리하는 Entity라는 의미
+//@Table(name = "item") 객체명과 같으면 생략해도된다.
 public class Item {
 
+    @Id /*pk*/ @GeneratedValue(strategy = GenerationType.IDENTITY) //pk의 값이 IDENTITY전략으로 생성(db에서 값을 넣어주는 전략)
     private Long id;
 
+    //이 필드는 db의 item_name 컬럼과 매핑된다는 의미 컬럼명과 테이블명이 같으면 비워도 된다.
+    @Column(name = "item_name", length=10)
     private String itemName;
+
     private Integer price;
     private Integer quantity;
 

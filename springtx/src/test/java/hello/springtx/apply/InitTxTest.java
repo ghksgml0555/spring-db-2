4539cfb633861cb9b48a6d78a49c1046f1b8b724
dsad2221s <ghksgml0555@naver.com>
2023-03-16 13:38:27 +0900

    @PostConstruct가 붙으면 트랜잭션이 안되기때문에  @EventListener(ApplicationReadyEvent.class)사용
    @PostConstruct가 붙으면 트랜잭션이 안되기때문에  @EventListener(ApplicationReadyEvent.class)사용

--- springtx/src/test/java/hello/springtx/apply/InitTxTest.java
+++ springtx/src/test/java/hello/springtx/apply/InitTxTest.java
@@ -1,5 +1,56 @@
 package hello.springtx.apply;
 
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.event.EventListener;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
+
+import javax.annotation.PostConstruct;
+
+@SpringBootTest
 public class InitTxTest {
-    
+
+    @Autowired Hello hello;
+
+    @Test
+    void go(){
+        //초기화 코드는 스프링이 초기화 시점에 호출한다.(hello를 빈에 등록하고 @PostConstruct
+        //때문에 초기화 메서드가 호출됨)
+
+    }
+
+    @TestConfiguration
+    static class InitTxTestConfig{
+        @Bean
+        Hello hello(){
+            return new Hello();
+        }
+    }
+
+    @Slf4j
+    static class Hello{
+        
+        @PostConstruct
+        @Transactional
+        public void initV1(){
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init @PostConstruce tx active={}", isActive);
+        }
+
+        //@EventListener > 스프링이 적절한 타이밍에 호출해준다.
+        //ApplicationReadyEvent.class >>스프링컨테이너가 완전히 떳을때 호출
+        @EventListener(ApplicationReadyEvent.class)
+        @Transactional
+        public void initV2(){
+            boolean isActive = TransactionSynchronizationManager.isActualTransactionActive();
+            log.info("Hello init ApplicationReadyEvent tx active={}", isActive);
+        }
+    }
+
 }
